5ee6aaee2deb18155963260958951193a7414faf
SaeedMawed <saeed.moued@gmail.com>
2020-06-03 13:53:33 +0300

    first commit design-portfolio theme

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var direction = document.getElementsByTagName("BODY")[0].getAttribute("dir");
+
+function openDashboard() {
+    var dashboard = document.getElementById("dashboardSection");
+    var open = document.getElementById("dashboard-open-btn");
+    var close = document.getElementById("dashboard-close-btn");
+    if (direction === "rtl") {
+        dashboard.style.marginRight = "0px";
+    } else {
+        dashboard.style.marginLeft = "0px";
+    }
+
+    open.style.display = "none";
+    close.style.display = "block";
+}
+
+function closeDashboard() {
+    var dashboard = document.getElementById("dashboardSection");
+    var open = document.getElementById("dashboard-open-btn");
+    var close = document.getElementById("dashboard-close-btn");
+    if (direction == "rtl") {
+        dashboard.style.marginRight = "-350px";
+    } else {
+        dashboard.style.marginLeft = "-350px";
+    }
+    open.style.display = "block";
+    close.style.display = "none";
+}
+
+$(window).resize(function() {
+    if ($(window).width() < 768) {} else {
+        openDashboard();
+    }
+});
+
+/* Start scroll up-down controller */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < currentScrollPos) {
+        document.getElementById("my-header-top").style.marginTop = "-40px";
+    } else {
+        document.getElementById("my-header-top").style.marginTop = "0px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+/* End scroll up-down controller */
---
+++ js/myswiper.js
@@ -0,0 +1,60 @@
+var swiper_3d = new Swiper(".swiper-3d", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: false,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper1 = new Swiper(".swiper-cube", {
+    effect: "cube",
+    grabCursor: true,
+    loop: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
