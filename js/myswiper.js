4d2d8f4924096e5c0311af6292371ec3e14b0c8a
SaeedMawed <saeed.moued@gmail.com>
2020-07-02 14:05:43 +0300

    remove audio file

--- js/myswiper.js
+++ js/myswiper.js
@@ -1,27 +1,4 @@
-var swiper_3d = new Swiper(".swiper-3d", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-});
-
-var swiper = new Swiper(".swiper-container", {
+var swiper_slide_conteainer = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 30,
   //effect: "fade",
@@ -45,7 +22,7 @@ var swiper = new Swiper(".swiper-container", {
   },
 });
 
-var swiper1 = new Swiper(".swiper-cube", {
+var swiper_cube = new Swiper(".swiper-cube", {
   effect: "cube",
   grabCursor: true,
   loop: true,
@@ -54,6 +31,29 @@ var swiper1 = new Swiper(".swiper-cube", {
     slideShadows: true,
     shadowOffset: 20,
     shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 25000000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper_3d = new Swiper(".swiper-3d", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
   },
   pagination: {
     el: ".swiper-pagination",
